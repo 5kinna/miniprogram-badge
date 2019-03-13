537b9012acd500bd1f7ce8ec83527b6c567af775
root <root@LAPTOP-F0R76G5L.localdomain>
2019-03-13 16:05:49 +0800

    feat: 添加提交代码检测

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'at-rule-empty-line-before': null,
+    'no-missing-end-of-source-newline': null,
+    'rule-empty-line-before': null,
+  },
+}
